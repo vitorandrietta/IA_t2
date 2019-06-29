8e282fe9800cb6420201aebba740cc24f7951bc7
andrietta <vitorandrietta@gmail.com>
2019-06-29 14:32:31 -0300

    typo corrected

--- core/kmeans/core.py
+++ core/kmeans/core.py
@@ -1,30 +1,18 @@
-class KmeansBase():
-    def cluster(self):
-        pass
-
-    def distance(self):
-        pass
-
-    def centro(self):
-        pass
-
-    def callback(self):
-        pass
-
-
-# A minha ideia (Édolas)
 class Point:
     def __init__(self, coordinates):
         self.coordinates = coordinates
+
 
 class Cluster:
     def __init__(self, center, points):
         self.center = center
         self.points = points
 
+
 class BaseProblemSolver:
     def __init__(self, img_path):
         self.img_path = img_path
+        self.clusters = []
 
     def distance(self, p, q):
         pass
@@ -40,21 +28,23 @@ class BaseProblemSolver:
         # PODEMOS IMPLEMENTAR AQUI, ACHO Q FIT NAO MUDAREMOS
 
     def is_fit_enough(self):
-        return true
+        return True
         #### SABER SE DEVEMOS PARAR DE RE CLUSTERIZAR
 
     def get_image_points(self):
         pass
         # TODO IMPLEMENTAR AQUI, NAO DEVE MUDAR
 
+
 class EuclidianDistanceProblemSolver(BaseProblemSolver):
     def distance(self, p, q):
         return 0
 
+
 ########## AI PRA FAZER AS COISAS VISUAIS, MUDAR OUTRO ARQUIVO #######
 
 class Runner:
-    def __init__(self, img_path)
+    def __init__(self, img_path):
         self.problem_solver = EuclidianDistanceProblemSolver()
 
     def run(self):
