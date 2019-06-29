1c5ba86c76516d1e6766d19a0825908c12a56160
andrietta <vitorandrietta@gmail.com>
2019-06-29 17:59:31 -0300

    test added

--- core/k_test.py
+++ core/k_test.py
@@ -2,7 +2,6 @@ from core.kmeans import core
 
 # def __init__(self, ncluster, img_path, mindif):
 
-
 if __name__ == '__main__':
     runner = core.Runner(5, "/home/andrietta/PycharmProjects/IA_t4/images/750x750bb.png", 3)
     res = runner.run()
