337c05b890eb8cd90aa7f1aede341a94ba63872a
andrietta <vitorandrietta@gmail.com>
2019-06-30 00:17:59 -0300

    pallets generate

--- core/k_test.py
+++ core/k_test.py
@@ -3,6 +3,6 @@ from core.kmeans import core
 # def __init__(self, ncluster, img_path, mindif):
 
 if __name__ == '__main__':
-    runner = core.Runner(5, "../images/nubank-22246-2.jpg", 1)
+    runner = core.Runner(5, "../images/kinvo.png", 1)
     res = runner.run()
     print(res)
